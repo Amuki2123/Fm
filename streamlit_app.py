9a50cdc4154170f551a8590e929956daa3055389
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2025-03-13 14:58:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,27 +1,4 @@
 import streamlit as st
-import seaborn as sns
-import pickle
-import torch
-import joblib
-import json
-import matplotlib.pyplot as plt
-from prophet import Prophet
-from neuralprophet import NeuralProphet
-from statsmodels.tsa.holtwinters import ExponentialSmoothing
-from keras.models import model_from_json
-from fbprophet import Prophet
-from prophet.serialize import model_to_json, model_from_json
-from neuralprophet import NeuralProphet, set_log_level
-from sklearn.preprocessing import MinMaxScaler
-import statsmodels.api as sm
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.tsa.arima_model import ARIMAResults
-import pmdarima as pm
-from statsmodels.tsa.stattools import adfuller, kpss
-from statsmodels.tsa.stattools import acf
-from statsmodels.tsa.stattools import pacf
-from statsmodels.tsa.seasonal import seasonal_decompose
-from statsmodels.graphics.tsaplots import plot_predict
 
 # Title of the app
 st.title(" 🎈Regional Malaria Cases Forecasting Models🎈")
