eb2d38e1f2c1c3361e77a71f1151037e55a4c96d
Amuki2123 <107396098+Amuki2123@users.noreply.github.com>
2025-03-13 15:58:15 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,4 @@ import streamlit as st
 st.title(" 🎈Regional Malaria Cases Forecasting Models🎈")
 st.info("Forecast malaria cases for Juba, Yei, and Wau based on rainfall and temperature using various models.")
 
-# Upload dataset
-uploaded_file = st.file_uploader("Upload a CSV file", type="csv")
-if uploaded_file:
-    data = pd.read_csv(uploaded_file)
-    st.write(data)
-
-    # Preprocess uploaded data
-    data['Date'] = pd.to_datetime(data['Date'])
-    data.set_index('Date', inplace=True)
 
